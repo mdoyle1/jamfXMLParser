7665837eccd56323666c5b4353f1385fd80e910f
Doyle, Mark(Information Technology Services) <doylema@eitsackgg7v.local>
2018-12-18 11:14:50 -0500

    Initial Commit

---
+++ jamfAPI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  jamfAPI
+//
+//  Created by Doyle, Mark(Information Technology Services) on 12/18/18.
+//  Copyright © 2018 Doyle, Mark(Information Technology Services). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ jamfAPI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  jamfAPI
+//
+//  Created by Doyle, Mark(Information Technology Services) on 12/18/18.
+//  Copyright © 2018 Doyle, Mark(Information Technology Services). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
